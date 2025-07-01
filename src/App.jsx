09a944caf6f0980f3377a13da4f8bbb43040f723
Charan27798 <saicharannallamasa27@gmail.com>
2025-07-01 21:13:11 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ const HandleEditTodo = (index)=>
     <div>
                   <h1> Todo App...</h1>
                   <h1> Hi...Github</h1>
-                  <h1>branches create chesa bro...</h1>
+                  <h1>branches create chesa....kdfgkodfmkgodfnkgofd.,,,,,,..</h1>
                   <input
                         type='text'
                         placeholder='Enter Your Todo here...'
